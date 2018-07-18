3108016056de842c11f7ab2dd8299d933a778719
Yash Raj <36835879+devrra@users.noreply.github.com>
2018-07-18 13:58:57 +0530

    Update FormingMagicSquare.cpp

--- FormingMagicSquare.cpp
+++ FormingMagicSquare.cpp
@@ -60,26 +60,17 @@ int formingMagicSquare(vector<vector<int>> s)
     return changes;
 }
 
+
 int main()
 {
-    ofstream fout(getenv("OUTPUT_PATH"));
-
     vector<vector<int>> s(3);
-    for (int i = 0; i < 3; i++) {
-        s[i].resize(3);
-
-        for (int j = 0; j < 3; j++) {
-            cin >> s[i][j];
-        }
-
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    }
-
+    for(int i=0; i<3; i++)
+    {
+        s[i] = vector<int>(3);
+        for(int j=0; j<3; j++)
+            cin>>s[i][j];
+    }    
     int result = formingMagicSquare(s);
-
-    fout << result << "\n";
-
-    fout.close();
-
+    cout << result << "\n";
     return 0;
 }
